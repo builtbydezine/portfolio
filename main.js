a00f9e9bc982f3a63c982488587484089543a9c2
Parth Gupta <builtbydezine@gmail.com>
2018-06-02 01:41:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -47,11 +47,9 @@ $.ajax({
     type: 'GET',
     success: function (data) {
         var myProjects = data.projects;
-        console.log('before:', myProjects);
             if (myProjects.length > 0) {
-                console.log('after:', myProjects);
-                $.each(function (i, val) {
-                    console.log('is', i);
+                myProjects.each(function (i, val) {
+                    console.log('is', myProject[i]);
                     $('#behance').prepend(
                         '<a class="shot" target="_blank" href="' + val.projects.url + '" title="' + val.projects.name + '"><div class="title">' + val.projects.name + '</div><img src="' + val.covers[404] + '"/></a>'
                     )
