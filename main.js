5c0c8b3e8ab00d3ff5577673b1dd0ef31aceca88
Parth Gupta <builtbydezine@gmail.com>
2018-06-01 22:52:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -32,34 +32,6 @@ jQuery(document).ready(function($){
         }, 150);
 
 
-
-
-
-// Set the Access Token
-var accessToken = '2865fd485e757f3f12c81bc63e591bdc598353493cd1eb622e250fae6109d33f';
-
-// Call Dribble v2 API
-$.ajax({
-    url: 'https://api.dribbble.com/v2/user/shots?access_token='+accessToken,
-    dataType: 'json',
-    type: 'GET',
-    success: function(data) {  
-      if (data.length > 0) { 
-        $.each(data.reverse(), function(i, val) {                
-          $('#dribbble').prepend(
-            '<a class="shot" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="'+ val.images.hidpi +'"/></a>'
-            )
-        })
-      }
-      else {
-        $('#dribbble').append('<p>No shots yet!</p>');
-      }
-    }
-});
-
-
-
-
         var g = a(".project-assets").children("div");
         a.each(g, function(b, c) {
             var d = a(this);
@@ -84,5 +56,43 @@ $.ajax({
         })
     })
 }(window.jQuery);
+
+// Set the Access Token
+var accessToken = '9f061d26c5a8be96b17a81718959a67dd54ca9669ca41752777193f7cc5be7c3';
+
+// Call Dribble v2 API
+$.ajax({
+    url: 'https://api.dribbble.com/v2/user/shots?access_token='+accessToken,
+    dataType: 'json',
+    type: 'GET',
+    success: function(data) {  
+      if (data.length > 0) { 
+        $.each(data.reverse(), function(i, val) {                
+          $('#shots').prepend(
+            '<a class="shot" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="'+ val.images.hidpi +'"/></a>'
+            )
+        })
+      }
+      else {
+        $('#shots').append('<p>No shots yet!</p>');
+      }
+    }
+});
+
+
+// API Variables
+
+//Image width 800x600 (Animated)
+  // val.images.hidpi 
+//Image width 400x300
+  // val.images.normal
+//Image width 200x150
+  // val.images.teaser
+//Title
+  // val.title
+//Description
+  // val.description
+//URL
+  // val.html_url
 
  
