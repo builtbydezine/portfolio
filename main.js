2ec1716f5e0d9200f2618f5b6289f8539deaee57
Parth Gupta <builtbydezine@gmail.com>
2018-06-01 23:53:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-jQuery(document).ready(function($){
-	var isLateralNavAnimating = false;
-	
-	//open/close lateral navigation
-	$('.cd-nav-trigger').on('click', function(event){
-		event.preventDefault();
-		//stop if nav animation is running 
-		if( !isLateralNavAnimating ) {
-			if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true; 
-			
-			$('body').toggleClass('navigation-is-open');
-			$('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-				//animation is over
-				isLateralNavAnimating = false;
-			});
-		}
-	});
-
-});
-
-
 ! function(a) {
     "use strict";
     a(document).ready(function() {
@@ -30,26 +9,6 @@ jQuery(document).ready(function($){
         setTimeout(function() {
             f.addClass("loaded")
         }, 150);
-
-
-        var g = a(".project-assets").children("div");
-        a.each(g, function(b, c) {
-            var d = a(this);
-            d.imagesLoaded(function() {
-                setTimeout(function() {
-                    d.addClass("loaded")
-                }, 50 + 150 * b)
-            })
-        });
-        var h = a(".projects-feed .project");
-        a.each(h, function(b, c) {
-            var d = a(this);
-            d.imagesLoaded(function() {
-                setTimeout(function() {
-                    d.addClass("loaded")
-                }, 50 + 50 * b)
-            })
-        });
         var i;
         a("a").on("click", function(a) {
             return "" == this.href || null == this.href ? void a.preventDefault() : void(-1 == this.href.indexOf("#") && -1 == this.href.indexOf("mailto:") && -1 == this.href.indexOf("javascript:") && "_blank" != this.target && (a.preventDefault(), i = this.href, f.removeClass("loaded"), setTimeout(b, 250)))
@@ -57,8 +16,10 @@ jQuery(document).ready(function($){
     })
 }(window.jQuery);
 
-// Set the Access Token
+ // Set the Access Token
 var accessToken = '2865fd485e757f3f12c81bc63e591bdc598353493cd1eb622e250fae6109d33f';
+var userID = 'builtbydezine';
+var behanceApiKey = 'mi5pbmpy0xX6lYkhyfZBtaR70SkEeGZn';
 
 // Call Dribble v2 API
 $.ajax({
@@ -70,6 +31,26 @@ $.ajax({
         $.each(data.reverse(), function(i, val) {                
           $('#shots').prepend(
             '<a class="shot" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="'+ val.images.hidpi +'"/></a>'
+            )
+        })
+      }
+      else {
+        $('#shots').append('<p>No shots yet!</p>');
+      }
+    }
+});
+var size = '202';
+
+// Call Behance Dev API
+$.ajax({
+    url: 'https://api.behance.net/v2/users/builtbydezine/projects?client_id=mi5pbmpy0xX6lYkhyfZBtaR70SkEeGZn',
+    dataType: 'json',
+    type: 'GET',
+    success: function(data) {  
+      if (data.length > 0) { 
+        $.each(data.reverse(), function(i, val) {                
+          $('#shots').prepend(
+            '<a class="shot" target="_blank" href="'+ val.projects.url +'" title="' + val.projects.name + '"><div class="title">' + val.projects.name + '</div><img src="'+ val.covers[404] +'"/></a>'
             )
         })
       }
@@ -95,4 +76,8 @@ $.ajax({
 //URL
   // val.html_url
 
- 
+
+
+
+
+  
