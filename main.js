1890cc34c39e23427e3409fa2c7b34729a158242
Parth Gupta <builtbydezine@gmail.com>
2018-06-02 00:14:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ $.ajax({
 
 // Call Behance Dev API
 $.ajax({
-    url: 'https://api.behance.net/v2/users/builtbydezine/projects?client_id=mi5pbmpy0xX6lYkhyfZBtaR70SkEeGZn',
+    url: 'https://api.behance.net/v2/users/' + userID + '/projects?client_id='+behanceApiKey,
     dataType: 'json',
     type: 'GET',
     success: function(data) {  
