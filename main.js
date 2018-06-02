37f3ae7c66a692c8ed086d1f80eb5d12a6f0c921
Parth Gupta <builtbydezine@gmail.com>
2018-06-02 17:02:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,53 +1,56 @@
-! function ($) {
+
+
+
+(function($) {
     "use strict";
     $(document).ready(function () {
-        
-        // Set the Access Token
-var accessToken = '2865fd485e757f3f12c81bc63e591bdc598353493cd1eb622e250fae6109d33f';
-var userID = 'builtbydezine';
-var behanceApiKey = 'mi5pbmpy0xX6lYkhyfZBtaR70SkEeGZn';
 
-// Call Dribble v2 API
-$.ajax({
-    url: 'https://api.dribbble.com/v2/user/shots?per_page=100&access_token=' + accessToken,
-    dataType: 'json',
-    type: 'GET',
-    success: function (data) {
-        if (data.length > 0) {
-            $.each(data.reverse(), function (i, val) {
-                $('#dribbble').prepend(
-                    '<a class="shot" target="_blank" href="' + val.html_url + '" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="' + val.images.hidpi + '"/></a>'
-                )
-            })
-        }
-        else {
-            $('#dribbble').append('<p>No shots yet!</p>');
-        }
-    }
-});
-
-// Call Behance Dev API
-$.ajax({
-    url: 'https://api.behance.net/v2/users/' + userID + '/projects?per_page=100&callback=?&client_id=' + behanceApiKey,
-    dataType: 'json',
-    type: 'GET',
-    success: function (data) {
-        var myProjects = data.projects;
-        if (myProjects.length > 0) {
-            $.each(myProjects.reverse(), function (i, val) {
-                $('#behance').prepend(
-                    '<a class="shot" target="_blank" href="' + val.url + '" title="' + val.name + '"><div class="title">' + val.name + '</div><img src="' + val.covers[404] + '"/></a>'
-                )
-            })
-        }
-        else {
-            $('#behance').append('<p>Behance Says, No shots yet!</p>');
-        }
-    }
-});
-        
         var year = new Date().getFullYear();
         $('.year').append(year);
+
+        // Set the Access Token
+        var accessToken = '2865fd485e757f3f12c81bc63e591bdc598353493cd1eb622e250fae6109d33f';
+        var userID = 'builtbydezine';
+        var behanceApiKey = 'mi5pbmpy0xX6lYkhyfZBtaR70SkEeGZn';
+
+        // Call Dribble v2 API
+        $.ajax({
+            url: 'https://api.dribbble.com/v2/user/shots?per_page=100&access_token=' + accessToken,
+            dataType: 'json',
+            type: 'GET',
+            success: function (data) {
+                if (data.length > 0) {
+                    $.each(data.reverse(), function (i, val) {
+                        $('#dribbble').prepend(
+                            '<a class="shot" target="_blank" href="' + val.html_url + '" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="' + val.images.hidpi + '"/></a>'
+                        )
+                    })
+                }
+                else {
+                    $('#dribbble').append('<p>No shots yet!</p>');
+                }
+            }
+        });
+
+        // Call Behance Dev API
+        $.ajax({
+            url: 'https://api.behance.net/v2/users/' + userID + '/projects?per_page=100&callback=?&client_id=' + behanceApiKey,
+            dataType: 'json',
+            type: 'GET',
+            success: function (data) {
+                var myProjects = data.projects;
+                if (myProjects.length > 0) {
+                    $.each(myProjects.reverse(), function (i, val) {
+                        $('#behance').prepend(
+                            '<a class="shot" target="_blank" href="' + val.url + '" title="' + val.name + '"><div class="title">' + val.name + '</div><img src="' + val.covers[404] + '"/></a>'
+                        )
+                    })
+                }
+                else {
+                    $('#behance').append('<p>Behance Says, No shots yet!</p>');
+                }
+            }
+        });
 
         $('a[data-mail]').on('click', function (e) {
             e.preventDefault();
@@ -70,4 +73,9 @@ $.ajax({
             })
         });
     })
-}(window.jQuery);
+
+    $(window).on('load', function() {
+		$(".loader-wrapper").delay(1000).fadeOut(500);
+    });	
+    
+})(window.jQuery);
