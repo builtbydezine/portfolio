7937525940e11c423d49094d41d55b88137dd8b7
Parth Gupta <builtbydezine@gmail.com>
2018-06-02 16:07:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,42 +1,24 @@
-! function(a) {
-    "use strict";
-    a(document).ready(function() {
-        function b() {
-            window.location = i
-        }
-        var e = a(".site-title"),
-            f = a("body");
-        setTimeout(function() {
-            f.addClass("loaded")
-        }, 150);
-        var i;
-        a("a").on("click", function(a) {
-            return "" == this.href || null == this.href ? void a.preventDefault() : void(-1 == this.href.indexOf("#") && -1 == this.href.indexOf("mailto:") && -1 == this.href.indexOf("javascript:") && "_blank" != this.target && (a.preventDefault(), i = this.href, f.removeClass("loaded"), setTimeout(b, 250)))
-        })
-    })
-}(window.jQuery);
-
- // Set the Access Token
+// Set the Access Token
 var accessToken = '2865fd485e757f3f12c81bc63e591bdc598353493cd1eb622e250fae6109d33f';
 var userID = 'builtbydezine';
 var behanceApiKey = 'mi5pbmpy0xX6lYkhyfZBtaR70SkEeGZn';
 
 // Call Dribble v2 API
 $.ajax({
-    url: 'https://api.dribbble.com/v2/user/shots?per_page=100&access_token='+accessToken,
+    url: 'https://api.dribbble.com/v2/user/shots?per_page=100&access_token=' + accessToken,
     dataType: 'json',
     type: 'GET',
-    success: function(data) {  
-      if (data.length > 0) { 
-        $.each(data.reverse(), function(i, val) {                
-          $('#dribbble').prepend(
-            '<a class="shot" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="'+ val.images.hidpi +'"/></a>'
-            )
-        })
-      }
-      else {
-        $('#dribbble').append('<p>Dribbble says, No shots yet!</p>');
-      }
+    success: function (data) {
+        if (data.length > 0) {
+            $.each(data.reverse(), function (i, val) {
+                $('#dribbble').prepend(
+                    '<a class="shot" target="_blank" href="' + val.html_url + '" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="' + val.images.hidpi + '"/></a>'
+                )
+            })
+        }
+        else {
+            $('#dribbble').append('<p>No shots yet!</p>');
+        }
     }
 });
 
@@ -47,38 +29,45 @@ $.ajax({
     type: 'GET',
     success: function (data) {
         var myProjects = data.projects;
-            if (myProjects.length > 0) {
-                $.each(myProjects.reverse(), function (i, val) {
-                    console.log('is', myProjects[i]);
-                    $('#behance').prepend(
-                        '<a class="shot" target="_blank" href="' + val.url + '" title="' + val.name + '"><div class="title">' + val.name + '</div><img src="' + val.covers[404] + '"/></a>'
-                    )
-                })
-            }
-            else {
-                $('#behance').append('<p>Behance Says, No shots yet!</p>');
-            }
+        if (myProjects.length > 0) {
+            $.each(myProjects.reverse(), function (i, val) {
+                $('#behance').prepend(
+                    '<a class="shot" target="_blank" href="' + val.url + '" title="' + val.name + '"><div class="title">' + val.name + '</div><img src="' + val.covers[404] + '"/></a>'
+                )
+            })
+        }
+        else {
+            $('#behance').append('<p>Behance Says, No shots yet!</p>');
+        }
     }
 });
 
 
-// API Variables
+! function ($) {
+    "use strict";
+    $(document).ready(function () {
+        var year = new Date().getFullYear();
+        $('.year').append(year);
 
-//Image width 800x600 (Animated)
-  // val.images.hidpi 
-//Image width 400x300
-  // val.images.normal
-//Image width 200x150
-  // val.images.teaser
-//Title
-  // val.title
-//Description
-  // val.description
-//URL
-  // val.html_url
+        $('a[data-mail]').on('click', function (e) {
+            e.preventDefault();
+            var domain = 'gmail.com';
+            window.location = 'mailto:' + $(this).data('mail') + '@' + domain.substring(0) + '?subject=Work Enquiry';
+        });
 
 
-
-
-
-  
+        var body = $("body");
+        setTimeout(function () {
+            body.addClass("loaded")
+        }, 150);
+        var shotContainer = $(".shot");
+        $.each(shotContainer, function (b, c) {
+            var $this = $(this);
+            $this.imagesLoaded(function () {
+                setTimeout(function () {
+                    $this.addClass("loaded")
+                }, 50 + 150 * b)
+            })
+        });
+    })
+}(window.jQuery);
