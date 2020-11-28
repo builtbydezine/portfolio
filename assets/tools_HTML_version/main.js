f188b08c38c808d6cc0fad105662f2f2947866f8
Parth Gupta <builtbydezine@gmail.com>
2020-11-28 13:42:27 +0530

    Update main.js

--- assets/tools_HTML_version/main.js
+++ assets/tools_HTML_version/main.js
@@ -1,55 +1,9 @@
-function fixedTableHeader(scrollTable) {
-
-  var bodyRows = scrollTable.querySelectorAll('.table tbody tr');
-  var bodyCells = bodyRows[0].querySelectorAll('td');
-
-  function setWidth() {
-    // Get the tbody columns width array
-    var colWidth = Object.keys(bodyCells).map(function (key) {
-      return [bodyCells[key].offsetWidth];
-    });
-
-    // Set the width of thead columns
-    var headCells = scrollTable.querySelectorAll("thead tr th");
-
-    Object.keys(headCells).forEach(function (key) {
-      headCells[key].style.width = colWidth[key][0] + 'px';
-    });
-  };
-  setWidth();
-
-
-  // add resize event
-  var addEvent = function (object, type, callback) {
-    if (object == null || typeof (object) == 'undefined') return;
-    if (object.addEventListener) {
-      object.addEventListener(type, callback, false);
-    } else if (object.attachEvent) {
-      object.attachEvent("on" + type, callback);
-    } else {
-      object["on" + type] = callback;
-    }
-  };
-  addEvent(window, "resize", setWidth);
-
-
-  //Add the modified thead
-  var thead = scrollTable.querySelector("thead")
-  //var tableScroll = document.querySelector('.scroll-table');
-  var clone = thead.cloneNode(true);
-
-  clone.className = "";
-  clone.classList.add('fixed-header');
-  scrollTable.insertBefore(clone, scrollTable.children[0]);
-}
-
-
 $(document).ready(function () {
   //$('[data-toggle="tooltip"]').tooltip();
 
   // Change the selector if needed
   var $table = $(".scroll-table");
-  /*  $bodyCells = $table.find("tbody tr:first").children(),
+    $bodyCells = $table.find("tbody tr:first").children(),
     colWidth;
 
   // Adjust the width of thead cells when window resizes
@@ -80,7 +34,7 @@ $(document).ready(function () {
       .clone()
       .addClass("fixed-header");
     $table.prepend(tableHead);
-*/
+
   $(".toggle-sidemodal a").on("click", function (e) {
     e.preventDefault();
     $(".sidemodal").toggleClass("shown");
